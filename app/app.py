717cce88a5aab8ea8ec1ecc8f6d8a18b7b476d47
FantasticApple <zhawhjw@users.noreply.github.com>
2025-02-09 23:57:56 -0800

    1st trial

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+# initialize Flask service
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello CPS3500!"
+
+@app.route("/new_page")
+def hi():
+    return "This is!"
+
+
+@app.route('/user/<username>')
+def user(username):
+    return render_template("user.html",
+                           username=username)
+if __name__ == "__main__":
+    app.run(debug=True)
+
